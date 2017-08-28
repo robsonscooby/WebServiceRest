1ca1cd8bb90bf8d84124c8bd92ee86cd0df6c352
Robson Carlos <robsonscooby@gmail.com>
2017-08-28 14:32:54 -0300

    teste rest service

--- src/main/java/br/com/celulasreligiosas/controller/NoticaController.java
+++ src/main/java/br/com/celulasreligiosas/controller/NoticaController.java
@@ -16,6 +16,7 @@ import javax.ws.rs.Produces;
 import br.com.celulasreligiosas.http.Noticia;
 import br.com.celulasreligiosas.repository.NoticiaRepository;
 import br.com.celulasreligiosas.repository.entity.NoticiaEntity;
+import br.com.celulasreligiosas.repository.entity.PessoaEntity;
 
 
 /**
@@ -26,156 +27,161 @@ import br.com.celulasreligiosas.repository.entity.NoticiaEntity;
 @Path("/serviceNoticia")
 public class NoticaController {
 		
-	private final  NoticiaRepository repository = new NoticiaRepository();
-
+//	private final  NoticiaRepository repository = new NoticiaRepository();
+//
+//	/**
+//	 * @Consumes - determina o formato dos dados que vamos postar
+//	 * @Produces - determina o formato dos dados que vamos retornar
+//	 * 
+//	 * Esse método cadastra uma nova notica
+//	 * */
+//	@POST	
+//	@Consumes("application/json; charset=UTF-8")
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/cadastrar")
+//	public String Cadastrar(Noticia noticia){
+//		
+//		NoticiaEntity entity = new NoticiaEntity();
+//				
+//		try {
+//
+//			entity.setAutor(noticia.getAutor());
+//			entity.setTitulo(noticia.getTitulo());
+//			entity.setDecricao(noticia.getDecricao());
+//			entity.setUrl(noticia.getUrl());
+//			//entity.setFoto(noticia.getFoto());
+//			
+//			repository.Salvar(entity);
+//			
+//			return "Registro cadastrado com sucesso!";
+//			
+//		} catch (Exception e) {
+//			
+//			return "Erro ao cadastrar um registro " + e.getMessage();
+//		}
+//	
+//	}
+//	
+//	/**
+//	 * Essse método altera uma noticia já cadastrada
+//	 * **/
+//	@PUT
+//	@Produces("application/json; charset=UTF-8")
+//	@Consumes("application/json; charset=UTF-8")	
+//	@Path("/alterar")
+//	public String Alterar(Noticia noticia){
+//		
+//		NoticiaEntity entity = new NoticiaEntity();
+//		
+//		try {
+//
+//			entity.setCodigo(noticia.getCodigo());
+//			entity.setAutor(noticia.getAutor());
+//			entity.setTitulo(noticia.getTitulo());
+//			entity.setDecricao(noticia.getDecricao());
+//			entity.setUrl(noticia.getUrl());
+//			//entity.setFoto(noticia.getFoto());
+//			
+//			repository.Alterar(entity);
+//			
+//			return "Registro alterado com sucesso!";
+//			
+//		} catch (Exception e) {
+//			
+//			return "Erro ao alterar o registro " + e.getMessage();
+//			
+//		}
+//
+//	}
+//	/**
+//	 * Esse método lista todas noticias cadastradas na base
+//	 * */
+//	@GET
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/todasNoticias")
+//	public List<Noticia> TodasNoticias(){
+//		
+//		List<Noticia> noticias =  new ArrayList<Noticia>();
+//		
+//		List<NoticiaEntity> listaEntityNoticias = repository.TodasNoticias();
+//		
+//		for (NoticiaEntity entity : listaEntityNoticias) {
+//			noticias.add(new Noticia(entity.getCodigo(), 
+//									 entity.getAutor(),
+//									 entity.getTitulo(), 
+//									 entity.getDecricao(),
+//									 entity.getUrl() //,entity.getFoto()
+//									 ));
+//		}
+//		
+//		return noticias;
+//	}
+//	
+//	/**
+//	 * Esse método busca uma notica cadastrada pelo código
+//	 * */
+//	@GET
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/getNoticia/{codigo}")
+//	public Noticia GetNoticia(@PathParam("codigo") Integer codigo){
+//		
+//		NoticiaEntity entity = repository.GetNoticia(codigo);
+//		
+//		if(entity != null)
+//			return new Noticia(entity.getCodigo(), 
+//							entity.getAutor(),
+//							entity.getTitulo(), 
+//							entity.getDecricao(),
+//							entity.getUrl() //, entity.getFoto()
+//							);
+//		
+//		return null;
+//	}
+//	
+//	/**
+//	 * Excluindo uma pessoa pelo código
+//	 * */
+//	@DELETE
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/excluir/{codigo}")	
+//	public String Excluir(@PathParam("codigo") Integer codigo){
+//		
+//		try {
+//			
+//			repository.Excluir(codigo);
+//			
+//			return "Registro excluido com sucesso!";
+//			
+//		} catch (Exception e) {
+//		
+//			return "Erro ao excluir o registro! " + e.getMessage();
+//		}
+//		
+//	}
+	
 	/**
 	 * @Consumes - determina o formato dos dados que vamos postar
 	 * @Produces - determina o formato dos dados que vamos retornar
 	 * 
-	 * Esse método cadastra uma nova notica
+	 * Esse método cadastra uma nova pessoa
 	 * */
-	@POST	
+	@GET	
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@Path("/cadastrar")
-	public String Cadastrar(Noticia noticia){
+	@Path("/teste")
+	public String teste(){
 		
-		NoticiaEntity entity = new NoticiaEntity();
+		PessoaEntity entity = new PessoaEntity();
 				
 		try {
-
-			entity.setAutor(noticia.getAutor());
-			entity.setTitulo(noticia.getTitulo());
-			entity.setDecricao(noticia.getDecricao());
-			entity.setUrl(noticia.getUrl());
-			//entity.setFoto(noticia.getFoto());
 			
-			repository.Salvar(entity);
-			
-			return "Registro cadastrado com sucesso!";
+			return "Web service funcionando!";
 			
 		} catch (Exception e) {
 			
 			return "Erro ao cadastrar um registro " + e.getMessage();
 		}
 	
-	}
-	
-	/**
-	 * Essse método altera uma noticia já cadastrada
-	 * **/
-	@PUT
-	@Produces("application/json; charset=UTF-8")
-	@Consumes("application/json; charset=UTF-8")	
-	@Path("/alterar")
-	public String Alterar(Noticia noticia){
-		
-		NoticiaEntity entity = new NoticiaEntity();
-		
-		try {
-
-			entity.setCodigo(noticia.getCodigo());
-			entity.setAutor(noticia.getAutor());
-			entity.setTitulo(noticia.getTitulo());
-			entity.setDecricao(noticia.getDecricao());
-			entity.setUrl(noticia.getUrl());
-			//entity.setFoto(noticia.getFoto());
-			
-			repository.Alterar(entity);
-			
-			return "Registro alterado com sucesso!";
-			
-		} catch (Exception e) {
-			
-			return "Erro ao alterar o registro " + e.getMessage();
-			
-		}
-
-	}
-	/**
-	 * Esse método lista todas noticias cadastradas na base
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/todasNoticias")
-	public List<Noticia> TodasNoticias(){
-		
-		List<Noticia> noticias =  new ArrayList<Noticia>();
-		
-		List<NoticiaEntity> listaEntityNoticias = repository.TodasNoticias();
-		
-		for (NoticiaEntity entity : listaEntityNoticias) {
-			noticias.add(new Noticia(entity.getCodigo(), 
-									 entity.getAutor(),
-									 entity.getTitulo(), 
-									 entity.getDecricao(),
-									 entity.getUrl() //,entity.getFoto()
-									 ));
-		}
-		
-		return noticias;
-	}
-	
-	/**
-	 * Esse método busca uma notica cadastrada pelo código
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/getNoticia/{codigo}")
-	public Noticia GetNoticia(@PathParam("codigo") Integer codigo){
-		
-		NoticiaEntity entity = repository.GetNoticia(codigo);
-		
-		if(entity != null)
-			return new Noticia(entity.getCodigo(), 
-							entity.getAutor(),
-							entity.getTitulo(), 
-							entity.getDecricao(),
-							entity.getUrl() //, entity.getFoto()
-							);
-		
-		return null;
-	}
-	
-	/**
-	 * Excluindo uma pessoa pelo código
-	 * */
-	@DELETE
-	@Produces("application/json; charset=UTF-8")
-	@Path("/excluir/{codigo}")	
-	public String Excluir(@PathParam("codigo") Integer codigo){
-		
-		try {
-			
-			repository.Excluir(codigo);
-			
-			return "Registro excluido com sucesso!";
-			
-		} catch (Exception e) {
-		
-			return "Erro ao excluir o registro! " + e.getMessage();
-		}
-		
-	}
-	
-	
-	/**
-	 *Metodo para teste
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/teste")	
-	public String teste(){
-		
-		try {
-			
-			return "Servico Noticia rest funcionando!";
-			
-		} catch (Exception e) {
-		
-			return "Erro no Noticia servico rest! " + e.getMessage();
-		}
-		
 	}
 	
 }
--- src/main/java/br/com/celulasreligiosas/controller/ServiceController.java
+++ src/main/java/br/com/celulasreligiosas/controller/ServiceController.java
@@ -26,140 +26,146 @@ import br.com.celulasreligiosas.repository.entity.PessoaEntity;
 @Path("/service")
 public class ServiceController {
 		
-	private final  PessoaRepository repository = new PessoaRepository();
-
+//	private final  PessoaRepository repository = new PessoaRepository();
+//
+//	/**
+//	 * @Consumes - determina o formato dos dados que vamos postar
+//	 * @Produces - determina o formato dos dados que vamos retornar
+//	 * 
+//	 * Esse método cadastra uma nova pessoa
+//	 * */
+//	@POST	
+//	@Consumes("application/json; charset=UTF-8")
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/cadastrar")
+//	public String Cadastrar(Pessoa pessoa){
+//		
+//		PessoaEntity entity = new PessoaEntity();
+//				
+//		try {
+//
+//			entity.setNome(pessoa.getNome());
+//			entity.setSexo(pessoa.getSexo());
+//			
+//			repository.Salvar(entity);
+//			
+//			return "Registro cadastrado com sucesso!";
+//			
+//		} catch (Exception e) {
+//			
+//			return "Erro ao cadastrar um registro " + e.getMessage();
+//		}
+//	
+//	}
+//	
+//	/**
+//	 * Essse método altera uma pessoa já cadastrada
+//	 * **/
+//	@PUT
+//	@Produces("application/json; charset=UTF-8")
+//	@Consumes("application/json; charset=UTF-8")	
+//	@Path("/alterar")
+//	public String Alterar(Pessoa pessoa){
+//		
+//		PessoaEntity entity = new PessoaEntity();
+//		
+//		try {
+//
+//			entity.setCodigo(pessoa.getCodigo());
+//			entity.setNome(pessoa.getNome());
+//			entity.setSexo(pessoa.getSexo());
+//			
+//			repository.Alterar(entity);
+//			
+//			return "Registro alterado com sucesso!";
+//			
+//		} catch (Exception e) {
+//			
+//			return "Erro ao alterar o registro " + e.getMessage();
+//			
+//		}
+//
+//	}
+//	/**
+//	 * Esse método lista todas pessoas cadastradas na base
+//	 * */
+//	@GET
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/todasPessoas")
+//	public List<Pessoa> TodasPessoas(){
+//		
+//		List<Pessoa> pessoas =  new ArrayList<Pessoa>();
+//		
+//		List<PessoaEntity> listaEntityPessoas = repository.TodasPessoas();
+//		
+//		for (PessoaEntity entity : listaEntityPessoas) {
+//			
+//			pessoas.add(new Pessoa(entity.getCodigo(), entity.getNome(),entity.getSexo()));
+//		}
+//		
+//		return pessoas;
+//	}
+//	
+//	/**
+//	 * Esse método busca uma pessoa cadastrada pelo código
+//	 * */
+//	@GET
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/getPessoa/{codigo}")
+//	public Pessoa GetPessoa(@PathParam("codigo") Integer codigo){
+//		
+//		PessoaEntity entity = repository.GetPessoa(codigo);
+//		
+//		if(entity != null)
+//			return new Pessoa(entity.getCodigo(), entity.getNome(),entity.getSexo());
+//		
+//		return null;
+//	}
+//	
+//	/**
+//	 * Excluindo uma pessoa pelo código
+//	 * */
+//	@DELETE
+//	@Produces("application/json; charset=UTF-8")
+//	@Path("/excluir/{codigo}")	
+//	public String Excluir(@PathParam("codigo") Integer codigo){
+//		
+//		try {
+//			
+//			repository.Excluir(codigo);
+//			
+//			return "Registro excluido com sucesso!";
+//			
+//		} catch (Exception e) {
+//		
+//			return "Erro ao excluir o registro! " + e.getMessage();
+//		}
+//		
+//	}
+	
 	/**
 	 * @Consumes - determina o formato dos dados que vamos postar
 	 * @Produces - determina o formato dos dados que vamos retornar
 	 * 
 	 * Esse método cadastra uma nova pessoa
 	 * */
-	@POST	
+	@GET	
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@Path("/cadastrar")
-	public String Cadastrar(Pessoa pessoa){
+	@Path("/teste")
+	public String teste(){
 		
 		PessoaEntity entity = new PessoaEntity();
 				
 		try {
-
-			entity.setNome(pessoa.getNome());
-			entity.setSexo(pessoa.getSexo());
 			
-			repository.Salvar(entity);
-			
-			return "Registro cadastrado com sucesso!";
+			return "Web service funcionando!";
 			
 		} catch (Exception e) {
 			
 			return "Erro ao cadastrar um registro " + e.getMessage();
 		}
 	
-	}
-	
-	/**
-	 * Essse método altera uma pessoa já cadastrada
-	 * **/
-	@PUT
-	@Produces("application/json; charset=UTF-8")
-	@Consumes("application/json; charset=UTF-8")	
-	@Path("/alterar")
-	public String Alterar(Pessoa pessoa){
-		
-		PessoaEntity entity = new PessoaEntity();
-		
-		try {
-
-			entity.setCodigo(pessoa.getCodigo());
-			entity.setNome(pessoa.getNome());
-			entity.setSexo(pessoa.getSexo());
-			
-			repository.Alterar(entity);
-			
-			return "Registro alterado com sucesso!";
-			
-		} catch (Exception e) {
-			
-			return "Erro ao alterar o registro " + e.getMessage();
-			
-		}
-
-	}
-	/**
-	 * Esse método lista todas pessoas cadastradas na base
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/todasPessoas")
-	public List<Pessoa> TodasPessoas(){
-		
-		List<Pessoa> pessoas =  new ArrayList<Pessoa>();
-		
-		List<PessoaEntity> listaEntityPessoas = repository.TodasPessoas();
-		
-		for (PessoaEntity entity : listaEntityPessoas) {
-			
-			pessoas.add(new Pessoa(entity.getCodigo(), entity.getNome(),entity.getSexo()));
-		}
-		
-		return pessoas;
-	}
-	
-	/**
-	 * Esse método busca uma pessoa cadastrada pelo código
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/getPessoa/{codigo}")
-	public Pessoa GetPessoa(@PathParam("codigo") Integer codigo){
-		
-		PessoaEntity entity = repository.GetPessoa(codigo);
-		
-		if(entity != null)
-			return new Pessoa(entity.getCodigo(), entity.getNome(),entity.getSexo());
-		
-		return null;
-	}
-	
-	/**
-	 * Excluindo uma pessoa pelo código
-	 * */
-	@DELETE
-	@Produces("application/json; charset=UTF-8")
-	@Path("/excluir/{codigo}")	
-	public String Excluir(@PathParam("codigo") Integer codigo){
-		
-		try {
-			
-			repository.Excluir(codigo);
-			
-			return "Registro excluido com sucesso!";
-			
-		} catch (Exception e) {
-		
-			return "Erro ao excluir o registro! " + e.getMessage();
-		}
-		
-	}
-	
-	/**
-	 *Metodo para teste
-	 * */
-	@GET
-	@Produces("application/json; charset=UTF-8")
-	@Path("/teste")	
-	public String teste(){
-		
-		try {
-			
-			return "Servico rest funcionando!";
-			
-		} catch (Exception e) {
-		
-			return "Erro no servico rest! " + e.getMessage();
-		}
-		
 	}
 	
 }
