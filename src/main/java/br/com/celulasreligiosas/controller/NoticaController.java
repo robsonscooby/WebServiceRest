d28edcefbedbcaa87943364d42105b7b1f8388a4
Robson Carlos <robsonscooby@gmail.com>
2017-08-30 22:54:16 -0300

    liberando envio de foto blob

--- src/main/java/br/com/celulasreligiosas/controller/NoticaController.java
+++ src/main/java/br/com/celulasreligiosas/controller/NoticaController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import br.com.celulasreligiosas.http.Noticia;
 import br.com.celulasreligiosas.repository.NoticiaRepository;
@@ -97,7 +98,8 @@ public class NoticaController {
 	 * Esse método lista todas noticias cadastradas na base
 	 * */
 	@GET
-	@Produces("application/json; charset=UTF-8")
+	//@Produces("application/json; charset=UTF-8")
+	@Produces(MediaType.MULTIPART_FORM_DATA)
 	@Path("/todasNoticias")
 	public List<Noticia> TodasNoticias(){
 		
